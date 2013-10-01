e638de0906d71f1dec16d546b667aaf955a88059
Philip Cohoe <cohoep@cat.pdx.edu>
2013-10-01 10:41:01 -0700

    Put the makesvn.pl chmod in the right block of an ifelse statement

--- makesvn.pl
+++ makesvn.pl
@@ -29,13 +29,13 @@ while ( my $row = $sth->fetchrow_hashref )
 
     if ( system("svnadmin create --fs-type fsfs ${svnroot}${identifier}" ) )
 	{
-    #This chmod is so that the projects website can access repos
-    system("chmod -R go+rX ${svnroot}${identifier}");
 	print $?, "\n";
 	print "something went boom\n";
 	}
     else
 	{
+    #This chmod is so that the projects website can access repos
+    system("chmod -R go+rX ${svnroot}${identifier}");
 	printf("Repository created: %s%s\n", $svnroot, $identifier);
 
     #setting the status is now handled later in update-gitolite.pl
